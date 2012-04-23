02758f621d9af49d3b7b573cd9bdcfeb802f78a6
Cory Green <greenmachine@fullsail.edu>
2012-04-22 23:12:49 -0700

    working on my main js file im disecting it so that I better understand what is going on

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -1,68 +1,29 @@
-/*
-	Could not implement my json data, for some reason it was just not working.  With time
-	Ill get it though. :D
+// this is the main java script file for the web-app
+// Author: Cory Green
+// Date: 03/18/2012
+// project: Mobile interfaces
 
-*/
-var parseMyInfo = function(data){
+//waiting until the DOM is ready
 
-// this is where the data can be parsed into a readable object for my json info.
-	console.log(data);
-};
-
-
-
-
-
-/*
-(function(storage)
+// function starts running when the dom is ready
+window.addEventListener("DOMContentLoaded", function()
 {
-	// need a unique key
-	var appName = "gig.bag";
-	
-	var canaryKey = appName + ".installed";
-	
-	// If this key exsists then we can bail out, as we already have initialized
-	if (storage.getItem(canaryKey) !== null)
+	// function that gets the element by id and returns it.
+	function getE(x)
 	{
-		return;
+		var theElement = document.getElementById(x);
+		return theElement;
 	}
-	
-	// otherwise, we need to populate local storage with dummy data!
-	var initialData = [
-		{
-			"fname": "Jon",
-			"bname": "Slayer",
-			"email": "your@email.com",
-			"startdate": "2012-12-10",
-			"groups": "rock"
-		}
-];
-}
-// read data will be more complex, but this is a simple example
-var populate = function(data)
-{
-	for (var i in data)
+
+	// upon clicking on the submit button, this will return the actual value submitted!
+	// the .value will return the actual value by ID
+	$("#submit").click(function()
 	{
-		var item = data[i];
-		var key = item.id;
-		var value = JSON.stringify(item);
-		storage.setItem(key, value);
-	}
-}
-*/
-
-
-
-
-$(document).ready(function(){
-
-	var rbform = $('#bandInfo')
-
-	rbform.validate({
-		invalidHandler: function(form, validator){},
-		submitHandler: function(){
-			var data = rbform.serializeArray();
-			parseMyInfo(data);
-		}
-	});
+		console.log(getE('fname').value);
+		console.log(getE('bname').value);
+		console.log(getE('email').value);
+	});	
 });
+
+
+
