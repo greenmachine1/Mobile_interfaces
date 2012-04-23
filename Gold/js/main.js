740756628c71a0d52212c5a3db5b8f88d8b81918
Cory Green <greenmachine@fullsail.edu>
2012-04-23 01:10:17 -0700

    First commit for week 4

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -19,9 +19,32 @@ window.addEventListener("DOMContentLoaded", function()
 	// the .value will return the actual value by ID
 	$("#submit").click(function()
 	{
-		console.log(getE('fname').value);
-		console.log(getE('bname').value);
-		console.log(getE('email').value);
+		// calls on the getE() and passes in the id name value and returns the actual value.
+		var name = (getE('fname').value);
+		var band = (getE('bname').value);
+		var email = (getE('email').value);
+		var groups = (getE('groups').value);
+		
+		var checkbox1, checkbox2, checkbox3, checkbox4, checkbox5,
+			checkbox6, checkbox7, checkbox8, checkbox9;
+		
+		
+		// dirty way of doing checkbox input but it works.
+		for(var i = 1; i < 10; i++)
+		{
+			if ('inst'+i.checked === true){
+				('checkbox'+i) == 'yes';
+			}else{
+				('checkbox'+i) == 'no';
+			}	
+		}
+			
+		
+		
+		alert('Your name is ' + name + ' and your bands name is ' + band + ' and your email is ' + email + 
+			  ' group selected ' + groups + ' checkbox ' + checkbox1 + ',' + checkbox2 + ',' + checkbox3 +
+			',' + checkbox4 + ',' + checkbox5 + ',' + checkbox6 + ',' + checkbox7 + ',' + checkbox8 + 
+			',' + checkbox9);
 	});	
 });
 
